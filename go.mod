05fe5eacb1cd74e5a6b7e8a5e0d81f5f049c1acc
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2023-12-09 13:10:29 +0330

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/ormushq/ormus
+
+go 1.21.0
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1
+
+require github.com/knadh/koanf v1.5.0
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ logger/logger.go
@@ -0,0 +1,56 @@
+package logger
+
+import (
+	"io"
+	"log/slog"
+	"os"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+const (
+	defaultFilePath        = "logs/logs.json"
+	defaultUseLocalTime    = false
+	defaultFileMaxSizeInMB = 10
+	defaultFileAgeInDays   = 30
+)
+
+type Config struct {
+	FilePath         string
+	UseLocalTime     bool
+	FileMaxSizeInMB  int
+	FileMaxAgeInDays int
+}
+
+var l *slog.Logger
+
+func init() {
+	fileWriter := &lumberjack.Logger{
+		Filename:  defaultFilePath,
+		LocalTime: defaultUseLocalTime,
+		MaxSize:   defaultFileMaxSizeInMB,
+		MaxAge:    defaultFileAgeInDays,
+	}
+	l = slog.New(
+		slog.NewJSONHandler(io.MultiWriter(fileWriter, os.Stdout), &slog.HandlerOptions{}),
+	)
+}
+
+func L() *slog.Logger {
+	return l
+}
+
+func New(cfg Config, opt *slog.HandlerOptions) *slog.Logger {
+	fileWriter := &lumberjack.Logger{
+		Filename:  cfg.FilePath,
+		LocalTime: cfg.UseLocalTime,
+		MaxSize:   cfg.FileMaxSizeInMB,
+		MaxAge:    cfg.FileMaxAgeInDays,
+	}
+
+	logger := slog.New(
+		slog.NewJSONHandler(io.MultiWriter(fileWriter, os.Stdout), opt),
+	)
+
+	return logger
+}

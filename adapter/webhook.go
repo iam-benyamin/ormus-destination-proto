f7cad9d1e97b290c7fc0165e3d5fa9535bd8dce8
Benyamin Mahmoudyan <benyaminmahmoudyan@gmail.com>
2023-12-10 22:29:13 +0330

    feat: baseic structure for destination

---
+++ adapter/webhook.go
@@ -0,0 +1,5 @@
+package adapter
+
+// TODO: implement webhook to connect with third party destinations if we have
+// webhook for destination and source and other packages then we have to create
+// destination directory for our worker

f9cca4dc073d484e764295a0954eb1ff05468349
etg-dev <taghinia.dev@gmail.com>
2024-01-05 11:14:35 +0330

    feat: add adapter and subscribe to event functionality

--- adapter/nats/adapter.go
+++ adapter/nats/adapter.go
@@ -1,1 +1,79 @@
 package nats
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"github.com/ormushq/ormus/pkg/richerror"
+	"runtime"
+	"time"
+)
+
+// Config is the configuration for NATS adapter.
+type Config struct {
+	MaxPending int           `koanf:"max_pending"`
+	Timeout    time.Duration `koanf:"timeout"`
+	Port       int           `koanf:"port"`
+	Password   string        `koanf:"password"`
+	User       string        `koanf:"user"`
+	Host       string        `koanf:"host"`
+}
+
+// Adapter represents the NATS adapter.
+type Adapter struct {
+	conn   *nats.Conn
+	config Config
+}
+
+// New creates a new NATS adapter.
+func New(config Config) (Adapter, error) {
+	const op = "nats.New"
+	url := fmt.Sprintf("nats://%s:%s@%s:%d", config.User, config.Password, config.Host, config.Port)
+	conn, err := nats.Connect(url)
+	if err != nil {
+		rErr := richerror.New(op).
+			WhitMessage("failed to connect to NATS server:").
+			WhitKind(richerror.KindUnexpected).
+			WhitWarpError(err)
+
+		return Adapter{}, rErr
+	}
+
+	return Adapter{conn: conn, config: config}, nil
+}
+
+func (a Adapter) Conn() *nats.Conn {
+	return a.conn
+}
+
+func (a Adapter) Close() {
+	if a.conn != nil {
+		a.conn.Close()
+	}
+}
+
+// Subscribe subscribes to the specified NATS topic and processes incoming events.
+func (a Adapter) Subscribe(topic string, action func(msg *nats.Msg)) error {
+	const op = "nats.Subscribe"
+
+	sub, err := a.conn.Subscribe(topic, action)
+	if err != nil {
+		rErr := richerror.New(op).
+			WhitMessage("Error subscribing to NATS subject").
+			WhitKind(richerror.KindUnexpected).
+			WhitWarpError(err)
+
+		return rErr
+	}
+
+	// TODO - here we should use ormus logger
+	fmt.Println("Subscribed to NATS subject")
+
+	defer func() {
+		if err := sub.Unsubscribe(); err != nil {
+			// TODO - Handle the error when unsubscribing and add a logger
+		}
+	}()
+
+	runtime.Goexit()
+	return nil
+}
--- adapter/nats/broker.go
+++ adapter/nats/broker.go
@@ -1,1 +1,32 @@
 package nats
+
+import (
+	"fmt"
+	"github.com/nats-io/nats.go"
+	"github.com/ormushq/ormus/event"
+)
+
+// SubscribeToEvent subscribes to the specified NATS subject and processes incoming events.
+func (a Adapter) SubscribeToEvent(topic string, action func(event *event.CoreEvent)) error {
+	const op = "nats.SubscribeToEvent"
+
+	// TODO - maybe we could only consider get topic from config for now
+
+	return a.Subscribe(topic, func(msg *nats.Msg) {
+		coreEvent, err := decodeCoreEvent(msg)
+		if err != nil {
+			// TODO - use ormus logger
+			fmt.Println("Error decoding CoreEvent:", err)
+			return
+		}
+
+		action(coreEvent)
+	})
+}
+
+// decodeCoreEvent decodes the Protobuf message into a CoreEvent struct.
+func decodeCoreEvent(msg *nats.Msg) (*event.CoreEvent, error) {
+	// TODO - Implement me
+	coreEvent := &event.CoreEvent{}
+	return coreEvent, nil
+}
